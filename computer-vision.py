d8669b29636badef0350133d2f669af711f28eb9
Emily <emilyli007@gmail.com>
2020-12-02 20:49:44 -0500

    modularized components

--- computer-vision.py
+++ computer-vision.py
@@ -24,7 +24,6 @@ def image_analysis(image_path, searching_object, confidence_threshold):
                                     visual_features=[
                                         VisualFeatureTypes.image_type,
                                         VisualFeatureTypes.categories,
-                                        # VisualFeatureTypes.color,
                                         VisualFeatureTypes.tags,
                                         VisualFeatureTypes.description
                                     ])
@@ -34,11 +33,28 @@ def image_analysis(image_path, searching_object, confidence_threshold):
 
     print("Tags associated with this image:\nTag\t\tConfidence")
     for tag in image_analysis.tags:
-        print("{}\t\t{}".format(tag.name, tag.confidence))
+        # print("{}\t\t{}".format(tag.name, tag.confidence))
         # keep image if contain desired tag value
-        if (tag.name == searching_object) and (tag.confidence >= confidence_threshold):
+        if (tag.name in searching_object) and (tag.confidence >= confidence_threshold):
+            print("{}\t\t{}".format(tag.name, tag.confidence))
             print("keeping image")
             return image_path
+    # when detected object is not the one we are searching for
+    print("No", searching_object)
+    return False
+
+def fix_orientation(image):
+    for orientation in ExifTags.TAGS.keys() : 
+        if ExifTags.TAGS[orientation]=='Orientation' : break 
+    exif=dict(image._getexif().items())
+
+    if exif[orientation] == 3 : 
+        image=image.rotate(180, expand=True)
+    elif exif[orientation] == 6 : 
+        image=image.rotate(270, expand=True)
+    elif exif[orientation] == 8 : 
+        image=image.rotate(90, expand=True)
+    return image
 
 # Function for object detection
 def object_detection(image_path):
@@ -71,23 +87,14 @@ def object_detection(image_path):
                                 facecolor='none',
                                 lw=2))
             image = Image.open(image_path)
-            # MODULARIZE THIS
-            for orientation in ExifTags.TAGS.keys() : 
-                if ExifTags.TAGS[orientation]=='Orientation' : break 
-            exif=dict(image._getexif().items())
+            image = fix_orientation(image)
 
-            if exif[orientation] == 3 : 
-                image=image.rotate(180, expand=True)
-            elif exif[orientation] == 6 : 
-                image=image.rotate(270, expand=True)
-            elif exif[orientation] == 8 : 
-                image=image.rotate(90, expand=True)
-
-            im1 = image.crop((object.rectangle.x, object.rectangle.y, object.rectangle.x + object.rectangle.w, object.rectangle.y + object.rectangle.h))
-            im1.thumbnail((1000,1000), Image.ANTIALIAS)
-            im1.save("output.jpeg", "JPEG")
+            analysis_result = image_analysis(image_path, "chair", 0.9)
+            if analysis_result != False: # could this be != null?
+                im1 = image.crop((object.rectangle.x, object.rectangle.y, object.rectangle.x + object.rectangle.w, object.rectangle.y + object.rectangle.h))
+                # im1.thumbnail((1000,1000), Image.ANTIALIAS)
+                im1.save("output.jpg", "JPEG")
             # im1.show()
-            image_analysis("C:/Users/lisiq.DESKTOP-6HN025I/Documents/Drone/output.jpeg", "chair", 0.9) #modularize!
 
     # plt.axis("off")
     plt.show()
@@ -96,3 +103,4 @@ local_image_path_objects = "C:/Users/lisiq.DESKTOP-6HN025I/Documents/Drone/full_
 local_image_objects = open(local_image_path_objects, "rb")
 
 object_detection(local_image_path_objects)
+# image_analysis(local_image_path_objects, "chair", 0.9)
