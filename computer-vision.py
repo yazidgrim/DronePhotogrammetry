ba78ed4706279d1679b9574c1b787211e7bb89c7
Emily <emilyli007@gmail.com>
2020-12-14 21:25:50 -0500

    formatting & colouring outputs and cleaning up

--- computer-vision.py
+++ computer-vision.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.patches import Rectangle
 from PIL import Image, ExifTags
+from colorama import Fore, Back, Style
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -34,16 +35,13 @@ def image_analysis(image_path):
         image_analysis.description.captions[0].text))
 
     for tag in image_analysis.tags:
-        # print("{}\t\t{}".format(tag.name, tag.confidence))
-        # keep image if contain desired tag value
         if (tag.name in searching_object) and (tag.confidence >= confidence_threshold):
             print("Tag\t\tConfidence")
-            print("{}\t\t{}".format(tag.name, tag.confidence))
-            print("keeping image")
+            print("{}\t\t{}\n".format(tag.name, tag.confidence))
+            print_c(Fore.GREEN, "keep object image\n")
             return True
  
-    print("Not", searching_object)
-    print("\n")
+    print_c(Fore.RED, "Not " + searching_object + '\n')
     return False
 
 def fix_orientation(image):
@@ -63,24 +61,19 @@ def fix_orientation(image):
 def object_detection(image_path):
     local_image_objects = open(image_path, "rb")
     detect_objects = computervision_client.detect_objects_in_stream(local_image_objects)
-    print("io object")
-    print(local_image_objects)
 
     # read in image to be plotted
     img = mpimg.imread(local_image_objects)
     plt.imshow(img)
 
-    print(detect_objects)
-
-    print("Detecting objects in image:")
+    print("\n\n##### Detecting objects in image: #####\n")
     if len(detect_objects.objects) == 0:
         print("No objects detected.")
     else:
-        file_dir = os.getcwd().replace("\\","/")
+        file_dir = os.getcwd().replace("\\","/")+"/DronePhotogrammetry/"
         for object in detect_objects.objects:
-            # print("Object")
-            # print(object)
-            print("Object label:", object.object_property)
+            print("Object label: ", end = '')
+            print_c(Fore.YELLOW, object.object_property)
             print("Confidence:", object.confidence)
 
             print("object at location {}, {}, {}, {}".format( \
@@ -100,11 +93,16 @@ def object_detection(image_path):
 
             cropped_img.save(output_image_name, "JPEG")
 
-            analysis_result = image_analysis(output_image_name) # need to use this
+            analysis_result = image_analysis(output_image_name)
             if analysis_result is not False:
                 cropped_img.save(file_dir+"/desired_object/output_"+str(datetime.now().strftime("%Y%m%d%H%M%S"))+".jpg", "JPEG")
+            print("-------------------")
 
-local_image_path_objects = os.getcwd().replace("\\","/")+"/chair_with_monitor.JPG"
+# Note: `color` is a color property from the colorama package
+def print_c(color, msg):
+    print(color + msg + Style.RESET_ALL)
+
+local_image_path_objects = os.getcwd().replace("\\","/")+"/DronePhotogrammetry/drone_photos/chair.jpg"
 local_image_objects = open(local_image_path_objects, "rb")
 
 object_detection(local_image_path_objects)
